2e050fe9d4d743b1b1d9de8a6d6b5b99d912c280
arun <jamestommy007@gmail.com>
2020-02-16 20:23:32 +0530

    Classes
    Signed-off-by: arun <jamestommy007@gmail.com>

---
+++ src/demo/Person.kt
@@ -0,0 +1,9 @@
+class Person {
+    var FirstName: String = ""
+    var LastName: String = ""
+    var MiddleName: String = ""
+
+    fun displayName() {
+        println("Name is ${FirstName} ${MiddleName} ${LastName}")
+    }
+}
--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,15 @@
 fun main(args: Array<String>) {
     println("Hello World!!!")
+    var person = Person()
+    person.FirstName = "Alan"
+    person.LastName = "Walker"
+    person.displayName()
+    var person1 = PersonWithConstructor("Jason", "", "Statham")
+    person1.display()
+}
+
+class PersonWithConstructor(var FirstName: String, var MiddleName: String, var LastName: String) {
+    fun display() {
+        println("Name is ${FirstName} ${MiddleName} ${LastName}")
+    }
 }
